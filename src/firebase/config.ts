32f4181c4649e14308647f509ba97680ea8cb5d2
ON Suryanarayanan <onsury@gmail.com>
2026-02-22 06:00:38 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-731784467-aba01",
+  "appId": "1:209880248926:web:1e422746debc353c24bacf",
+  "apiKey": "AIzaSyBZyNmlZ9V_abRwb3ktB1k-r6hy2nBcLTc",
+  "authDomain": "studio-731784467-aba01.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "209880248926"
+};
